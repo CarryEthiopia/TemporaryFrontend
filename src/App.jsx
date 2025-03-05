6d993fd1e3d88d69c67fe3d898764b8a9d6b1f3c
yabselam <yabsirahoji@gmail.com>
2025-03-05 10:07:05 -0800

    Added Terms and Policy layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing/Landingpage";
 import GetInTouch from "./pages/Landing/Contactus";
 import AskQuestions from "./pages/Landing/AskQuestions";
+import TermsAndPrivacy from "./pages/TermsAndPrivacy"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/get-in-touch" element={<GetInTouch />} />
         <Route path="/ask-questions" element={<AskQuestions />} />
+        <Route path="/terms-and-privacy" element={<TermsAndPrivacy />} />
         
       </Routes>
     </Router>
