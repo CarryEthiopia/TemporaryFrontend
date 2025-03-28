06c2eb8d83e90be612bedf273bfcc9367f43570e
BaslaelWorkineh <baslaworku@gmail.com>
2025-03-28 14:41:46 +0300

    SEO optimization done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing/Landingpage";
-import GetInTouch from "./pages/Landing/Contactus";
-import AskQuestions from "./pages/Landing/AskQuestions";
 import PrivacyPolicy from "./pages/privacyPolicy";
 import AccountDeletion from "./pages/accountDeletion";
 import TermsOfUse from "./pages/TermsofUse";
@@ -13,8 +11,6 @@ function App() {
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/get-in-touch" element={<GetInTouch />} />
-        <Route path="/ask-questions" element={<AskQuestions />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/account-deletion" element={<AccountDeletion />} />
         <Route path="/terms-of-use" element={<TermsOfUse />} />
